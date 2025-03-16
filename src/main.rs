e50af75ceb496b629e03c02a4a21c5bcf29c0f76
philipbroadway <me@philipbroadway.com>
2025-03-16 13:50:54 -0600

    learning about ownership & lifetime - syntax is a little unintuitve but concept is clear

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod search;
 
 use std::fs;
 use bible::Bible;
-// use search::find_verse;
+use search::find_verse;
 
 fn main() {
     let bible_json = include_str!("data/bible.json");
     let bible: Bible = serde_json::from_str(bible_json).expect("Failed to parse JSON");
 
-    println!("Bible loaded");
+    println!("Bible loaded with {} books.", bible.books.len());
 }
--- src/search.rs
+++ src/search.rs
@@ -1,8 +1,8 @@
-// use crate::bible::Bible;
+use crate::bible::Bible;
 
-// pub fn find_verse(bible: &Bible, book: &str, chapter: &str, verse: &str) -> Option<&str> {
-//     bible.books.get(book)?
-//         .chapter.get(&chapter)?
-//         .verse.get(&verse)
-//         .map(String::as_str)
-// }
+pub fn find_verse<'a>(bible: &'a Bible, book: &str, chapter: &str, verse: &str) ->  Option<&'a str> {
+    bible.books.get(book)?
+        .chapter.get(chapter)?
+        .verse.get(verse)
+        .map(String::as_str)
+}
