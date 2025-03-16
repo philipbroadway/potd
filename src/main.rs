6632a7dcc7e8b5e13cd9966dbf7eea27b6353db0
philipbroadway <me@philipbroadway.com>
2025-03-16 14:50:30 -0600

    cli param parsing

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,18 @@ fn main() {
 
   let args: Vec<String> = env::args().collect();
 
-  if args.len() != 4 {
-      eprintln!("Usage: {} <book> <chapter> <verse>", args[0]);
+  if args.len() > 0 {
+      eprintln!("------------------\nPassage of the day\n------------------");
+      eprintln!("\nOptions:\n{} -s <book> <chapter> <verse>", args[0]);
+      eprintln!("{} -q \"book chapter:verse[,-s]\"\n", args[0]);
       return;
   }
   
   //TODO: Setup CLI flags for different query options.
 
-  let book = &args[1].to_lowercase().replace(" ", "-");
-  let chapter = &args[2];
-  let verse = &args[3];
+  let book = &args[2].to_lowercase().replace(" ", "-");
+  let chapter = &args[3];
+  let verse = &args[4];
 
   let bible_json = include_str!("data/bible.json");
   let bible: Bible = serde_json::from_str(bible_json).expect("Failed to parse JSON");
