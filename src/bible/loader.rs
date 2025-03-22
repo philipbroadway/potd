b5e2cc575d0319bd0890bad49d3e6ad2b234f36c
philipbroadway <me@philipbroadway.com>
2025-03-21 22:19:45 -0600

    :broom:

--- src/bible/loader.rs
+++ src/bible/loader.rs
@@ -38,6 +38,30 @@ impl Chapter {
     }
 }
 
+impl BibleNumeric {
+  pub fn canonical_order(&self) -> Vec<String> {
+    let keys = [
+        "genesis", "exodus", "leviticus", "numbers", "deuteronomy",
+        "joshua", "judges", "ruth", "1-samuel", "2-samuel", "1-kings", "2-kings",
+        "1-chronicles", "2-chronicles", "ezra", "nehemiah", "esther", "job", "psalms",
+        "proverbs", "ecclesiastes", "song-of-solomon", "isaiah", "jeremiah", "lamentations",
+        "ezekiel", "daniel", "hosea", "joel", "amos", "obadiah", "jonah", "micah", "nahum",
+        "habakkuk", "zephaniah", "haggai", "zechariah", "malachi",
+        "matthew", "mark", "luke", "john", "acts", "romans", "1-corinthians", "2-corinthians",
+        "galatians", "ephesians", "philippians", "colossians", "1-thessalonians", "2-thessalonians",
+        "1-timothy", "2-timothy", "titus", "philemon", "hebrews", "james", "1-peter", "2-peter",
+        "1-john", "2-john", "3-john", "jude", "revelation",
+    ];
+    let mut ordered_books = Vec::new();
+    for &book in keys.iter() {
+        if self.books.contains_key(book) {
+            ordered_books.push(book.to_string());
+        }
+    }
+    ordered_books
+}
+}
+
 
 #[derive(Deserialize, Debug)]
 pub struct Bible {
@@ -68,3 +92,4 @@ pub struct BookNumeric {
 pub struct ChapterNumeric {
   pub verses: BTreeMap<u32, String>,
 }
+
